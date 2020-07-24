832ed12eec2b94cbfe8dc708649633cec83de191
Dzianis Hushcha <den.gushcha@gmail.com>
2020-07-24 19:50:33 +0300

    Add version 1.1 of parser

--- output_parser/parser.py
+++ output_parser/parser.py
@@ -1,13 +1,12 @@
 import re
-import json
 
 
 class Parser:
 
-    def build_result(serf, output, errors, return_code):
+    def build_result(self, output, errors, return_code):
         result = {
-            'error': serf.parse_errors(errors),
-            'result': serf.parse_output(output),
+            'error': self.parse_errors(errors),
+            'result': self.parse_output(output),
             'status': return_code,
         }
         return result
@@ -16,7 +15,16 @@ class Parser:
     def parse_output(iperf_output):
         diff_lines = re.split(r'\[.{3}\]', iperf_output)
         deleted_linebrakers = ''.join(diff_lines).split('\n')
-        result = [line.strip() for line in deleted_linebrakers if line]
+        lines = [line.strip() for line in deleted_linebrakers if line]
+        needed_lines = lines[2:13]
+        keys = needed_lines[0].split()
+        default_value = []
+        result = {key: list(default_value) for key in keys}
+        for index, key in enumerate(keys):
+            for line in needed_lines[1:]:
+                values = re.split(r' {2,}', line)
+                values = [' '.join(values[:2])] + values[2:]
+                result[key].append(values[index])
         return result
 
     @staticmethod
