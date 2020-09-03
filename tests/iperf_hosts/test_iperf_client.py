14bc4d8cfcb3dd0b4c62dfb89ea98f3c851f8f49
Dzianis Hushcha <den.gushcha@gmail.com>
2020-09-03 23:38:11 +0300

    draft variant for test_iperf_client.py

---
+++ tests/iperf_hosts/test_iperf_client.py
@@ -0,0 +1,38 @@
+import unittest
+from unittest import mock
+
+from iperf_hosts.iperf_client import IperfClient
+from executors.local_executor import LocalExecutor
+from executors.remote_executor import SSHExecutor
+
+
+class TestIperfServer(unittest.TestCase):
+    def setUp(self):
+        server_ip = '2.2.2.2'
+        self.command = f'iperf3 -c {server_ip}'
+        self.server = IperfClient(
+            'somehost@1.1.1.1',
+            'secret',
+            None,
+            server_ip
+        )
+
+    @mock.patch.object(SSHExecutor, 'execute')
+    def test_remote_measure(self, execute):
+        execute.return_value = 'test result'
+        with mock.patch.object(IperfClient, 'is_local', return_value=False):
+            result = self.server.measure()
+        execute.assert_called_once_with(self.command, check_output=True)
+        self.assertEqual(result, 'test result')
+
+    @mock.patch.object(LocalExecutor, 'execute')
+    def test_local_measure(self, execute):
+        execute.return_value = 'test result'
+        with mock.patch.object(IperfClient, 'is_local', return_value=True):
+            result = self.server.measure()
+        execute.assert_called_once_with(self.command, check_output=True)
+        self.assertEqual(result, 'test result')
+
+
+if __name__ == '__main__':
+    unittest.main()
